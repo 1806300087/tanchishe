9da8b8278cbf78c4620188f6304b42f0538fdfe4
zorozhou <zorozhou@tencent.com>
2025-03-26 15:28:52 +0800

    添加蛇的图片和动画效果

--- game.js
+++ game.js
@@ -11,6 +11,17 @@ class SnakeGame {
         this.speed = 150;
         this.isGameOver = false;
 
+        // 加载蛇的图片
+        this.snakeImages = {
+            head: new Image(),
+            body: new Image(),
+            tail: new Image()
+        };
+
+        this.snakeImages.head.src = 'images/snake-head.png';
+        this.snakeImages.body.src = 'images/snake-body.png';
+        this.snakeImages.tail.src = 'images/snake-tail.png';
+
         // 绑定按键事件
         document.addEventListener('keydown', this.handleKeyPress.bind(this));
         document.getElementById('startBtn').addEventListener('click', this.startGame.bind(this));
@@ -85,24 +96,67 @@ class SnakeGame {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         // 绘制蛇
-        this.ctx.fillStyle = '#4CAF50';
-        this.snake.forEach(segment => {
-            this.ctx.fillRect(
-                segment.x * this.gridSize,
-                segment.y * this.gridSize,
-                this.gridSize - 2,
-                this.gridSize - 2
-            );
+        this.snake.forEach((segment, index) => {
+            const x = segment.x * this.gridSize;
+            const y = segment.y * this.gridSize;
+            
+            // 保存当前上下文状态
+            this.ctx.save();
+            
+            // 移动到蛇段的位置
+            this.ctx.translate(x + this.gridSize/2, y + this.gridSize/2);
+            
+            // 根据方向旋转
+            let rotation = 0;
+            if (index === 0) { // 头部
+                switch(this.direction) {
+                    case 'up': rotation = -Math.PI/2; break;
+                    case 'down': rotation = Math.PI/2; break;
+                    case 'left': rotation = Math.PI; break;
+                    case 'right': rotation = 0; break;
+                }
+            } else if (index === this.snake.length - 1) { // 尾部
+                const prevSegment = this.snake[index - 1];
+                if (prevSegment.x < segment.x) rotation = 0;
+                else if (prevSegment.x > segment.x) rotation = Math.PI;
+                else if (prevSegment.y < segment.y) rotation = -Math.PI/2;
+                else if (prevSegment.y > segment.y) rotation = Math.PI/2;
+            } else { // 身体
+                const prevSegment = this.snake[index - 1];
+                const nextSegment = this.snake[index + 1];
+                if (prevSegment.x === nextSegment.x) {
+                    rotation = prevSegment.y < segment.y ? -Math.PI/2 : Math.PI/2;
+                } else {
+                    rotation = prevSegment.x < segment.x ? 0 : Math.PI;
+                }
+            }
+            
+            this.ctx.rotate(rotation);
+            
+            // 绘制蛇段
+            if (index === 0) {
+                this.ctx.drawImage(this.snakeImages.head, -this.gridSize/2, -this.gridSize/2, this.gridSize, this.gridSize);
+            } else if (index === this.snake.length - 1) {
+                this.ctx.drawImage(this.snakeImages.tail, -this.gridSize/2, -this.gridSize/2, this.gridSize, this.gridSize);
+            } else {
+                this.ctx.drawImage(this.snakeImages.body, -this.gridSize/2, -this.gridSize/2, this.gridSize, this.gridSize);
+            }
+            
+            // 恢复上下文状态
+            this.ctx.restore();
         });
 
         // 绘制食物
         this.ctx.fillStyle = '#FF0000';
-        this.ctx.fillRect(
-            this.food.x * this.gridSize,
-            this.food.y * this.gridSize,
-            this.gridSize - 2,
-            this.gridSize - 2
+        this.ctx.beginPath();
+        this.ctx.arc(
+            this.food.x * this.gridSize + this.gridSize/2,
+            this.food.y * this.gridSize + this.gridSize/2,
+            this.gridSize/2 - 2,
+            0,
+            Math.PI * 2
         );
+        this.ctx.fill();
     }
 
     gameOver() {
